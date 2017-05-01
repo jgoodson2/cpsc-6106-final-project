2c5c998b73a40890210b349f2c28d3e4d21c8d58
jgoodson2 <jonathongoodson@clayton.edu>
2017-05-01 14:39:41 -0400

    Add constructor to src/PieceWorker.java; add earnings() to src/PieceWorker.java

--- src/PieceWorker.java
+++ src/PieceWorker.java
@@ -1,3 +1,11 @@
-public class PieceWorker {
+public class PieceWorker extends Employee {
 
+    public PieceWorker(String first, String last, String ssn) {
+        super(first, last, ssn);
+    }
+
+    @Override
+    public double earnings() {
+        return 0;
+    }
 }
